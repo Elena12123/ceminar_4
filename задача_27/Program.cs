fb9dcbe3b70d4d462cfbffeaa5e243d86daac906
KEA <lena_konovalova@rambler.ru>
2022-09-04 19:03:42 +0300

    z27

---
+++ задача_27/Program.cs
@@ -0,0 +1,21 @@
+﻿int SumInNum ()
+{
+Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+int result = 0;
+// int A = num%100;
+// int B = A/10;
+// int C = (num - A)/100;
+// int D = num%10;
+
+while (num >0)
+{
+    result = result + num%10;
+    num = num/10;
+
+}
+return result;
+}
+Console.WriteLine (SumInNum ());
+
+
