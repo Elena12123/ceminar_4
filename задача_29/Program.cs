d4559e17307cdaa2c783f26082a8afa505ae4a7d
KEA <lena_konovalova@rambler.ru>
2022-09-05 16:03:49 +0300

    z29

---
+++ задача_29/Program.cs
@@ -0,0 +1,30 @@
+﻿void FillArray (int[] collection)
+{
+    int length = collection.Length;
+    int i = 0;
+
+    while (i < length)
+    {
+        collection[i] = new Random().Next(1, 100);
+        i++;
+    }
+
+
+}
+
+void PrintArray (int[] numbers)
+{
+    int count = numbers.Length;
+    int index = 0;
+    
+    while (index < count)
+    {
+        Console.WriteLine(numbers[index]);
+        index++;
+    }
+    
+}
+int [] array = new int[8];
+
+FillArray(array);
+PrintArray(array);
