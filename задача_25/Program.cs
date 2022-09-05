df907841e9d779a146948af198badac5e8f1de5d
KEA <lena_konovalova@rambler.ru>
2022-09-05 16:03:17 +0300

    z25

---
+++ задача_25/Program.cs
@@ -0,0 +1,13 @@
+﻿
+Console.WriteLine("Введите число A: ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B: ");
+int B = Convert.ToInt32(Console.ReadLine());
+int result = 1;
+
+for (int i = 1; i <= B; i++)
+{
+    result = result * A;
+}
+
+Console.WriteLine($"А в степени B = {result}");
